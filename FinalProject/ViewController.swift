0340bbd0040b507172df15166f33a2f9fe133b58
Liam Schmid <Liam@Liams-MacBook-Pro.local>
2017-10-15 17:33:11 -0700

    Initial Commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Liam Schmid on 10/15/17.
+//  Copyright © 2017 Liam Schmid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
